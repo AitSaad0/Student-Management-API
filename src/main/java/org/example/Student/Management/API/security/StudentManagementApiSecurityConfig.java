2d97c61d586b8c6654ad5d7c0a48c5a9df1ff9bb
Ait_SaaD <saadaitabd2004@gmail.com>
2025-11-22 18:49:01 +0100

    student management api security config

--- src/main/java/org/example/Student/Management/API/security/StudentManagementApiSecurityConfig.java
+++ src/main/java/org/example/Student/Management/API/security/StudentManagementApiSecurityConfig.java
@@ -1,4 +1,50 @@
 package org.example.Student.Management.API.security;
 
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
 public class StudentManagementApiSecurityConfig {
+
+    private final List<String> publicPaths;
+
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(csrfConfig -> csrfConfig.disable())
+                .authorizeHttpRequests((requests) -> {
+                    publicPaths.forEach(path -> requests.requestMatchers(path).permitAll());
+                    requests.anyRequest().authenticated();
+                })
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+        var daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
+        var providerManager = new ProviderManager(daoAuthenticationProvider);
+        return providerManager;
+    }
+
+
 }
