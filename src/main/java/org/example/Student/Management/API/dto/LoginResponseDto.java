5e4d23f4424c83800b71afb2e393e74d6f75fc2e
Ait_SaaD <saadaitabd2004@gmail.com>
2025-11-22 18:55:12 +0100

    add LoginResponseDto

--- src/main/java/org/example/Student/Management/API/dto/LoginResponseDto.java
+++ src/main/java/org/example/Student/Management/API/dto/LoginResponseDto.java
@@ -1,4 +1,11 @@
 package org.example.Student.Management.API.dto;
 
-public record LoginResponseDto() {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import org.example.Student.Management.API.entity.Student;
+
+public record LoginResponseDto(String message, StudentDto student, String jwt) {
 }
